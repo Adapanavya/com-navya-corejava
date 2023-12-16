29f16206177ff81ef8b803a32b1e616fcb27475a
Adapanavya <151008056+Adapanavya@users.noreply.github.com>
2023-12-16 14:36:03 +0530

    Delete monthNames/upperCase directory

--- monthNames/upperCase/UpperCase.java
+++
@@ -1,9 +0,0 @@
-package com.java.practice.upperCase;
-
-public class UpperCase {
-    public static void main(String[] args){
-        String s="Navya";
-        System.out.println(s.toUpperCase());
-        System.out.println(s.toLowerCase());
-    }
-}
