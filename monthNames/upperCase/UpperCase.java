71dd046ffa04bf8fb63bad018ab6a10c39236772
Adapanavya <151008056+Adapanavya@users.noreply.github.com>
2023-12-16 14:34:00 +0530

    Add files via upload

---
+++ monthNames/upperCase/UpperCase.java
@@ -0,0 +1,9 @@
+package com.java.practice.upperCase;
+
+public class UpperCase {
+    public static void main(String[] args){
+        String s="Navya";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+}
